b189a725d857386f2df1f0a1bf6e386880b27871
Drake Vorndran <DrakeVorndranOffical@gmail.com>
2018-10-25 10:46:37 -0700

    finished the game

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 const debug = document.getElementById("debug");
+let score = 0;
+let lives = 3;
 
 
 
@@ -35,11 +37,11 @@ const brickOffsetTop = 30;
 const brickOffsetLeft = brickPadding;
 
 
-var bricks = [];
-for(var c=0; c<brickColumnCount; c++) {
+let bricks = [];
+for(let c=0; c<brickColumnCount; c++) {
     bricks[c] = [];
-    for(var r=0; r<brickRowCount; r++) {
-        bricks[c][r] = { x: 0, y: 0 };
+    for(let r=0; r<brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
     }
 }
 
@@ -63,7 +65,7 @@ checkBallCollision = () => {
 
         ballDx*=-1;
     }
-    
+
     if(bally + ballDy - ballRadius < 0){
         ballDy*=-1;
     }
@@ -72,7 +74,17 @@ checkBallCollision = () => {
             ballDy*=-1;
         }
         else{
-            document.location.reload();
+            lives--;
+            if(lives===0){
+                alert("game over")
+                document.location.reload();
+            }
+            else{
+                ballx = canvas.width/2;
+                bally = canvas.height-30;
+                ballDx = 2;
+                ballDy = -2;
+            }
         }
     }
 
@@ -82,12 +94,19 @@ checkBallCollision = () => {
 moveBall = () => {
     ballx += ballDx;
     bally += ballDy;
-    checkBallCollision();
+    collision();
 }
 
 
 //PADDLE
 
+//document.addEventListener("mousemove", mouseMoveHandler, false); //This is here, but it completly ruins the momentum that I added, so I removed it
+function mouseMoveHandler(e) {
+    var relativeX = e.clientX - canvas.offsetLeft;
+    if(relativeX > 0 && relativeX < canvas.width) {
+        paddleX = relativeX - paddleWidth/2;
+    }
+}
 
 
 function drawPaddle() {
@@ -146,17 +165,38 @@ movePaddle = () => {
 //BRICKS
 
 function drawBricks() {
-    for(var c=0; c<brickColumnCount; c++) {
-        for(var r=0; r<brickRowCount; r++) {
-            var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
-            var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
-            bricks[c][r].x = brickX;
-            bricks[c][r].y = brickY;
-            ctx.beginPath();
-            ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillStyle = "#0095DD";
-            ctx.fill();
-            ctx.closePath();
+    for(let c=0; c<brickColumnCount; c++) {
+        for(let r=0; r<brickRowCount; r++) {
+            if(bricks[c][r].status===1){
+                let brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+                let brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+
+function brickCollision(){
+    for(let c=0; c<brickColumnCount; c++) {
+        for(let r=0; r<brickRowCount; r++) {
+            let b = bricks[c][r];
+            if(b.status===1){
+                if(ballx+ballRadius > b.x && ballx+ballRadius < b.x+brickWidth && bally+ballRadius > b.y && bally-ballRadius < b.y+brickHeight) {
+                    ballDy = -ballDy;
+                    b.status=0;
+                    score++;
+                    if(score == brickRowCount*brickColumnCount) {
+                        alert("YOU WIN, CONGRATULATIONS!");
+                        document.location.reload();
+                    }
+                }
+            }
         }
     }
 }
@@ -164,16 +204,38 @@ function drawBricks() {
 
 
 //DRAWING
+
+function drawScore() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Score: "+score, 8, 20);
+}
+
+
+function drawLives() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Lives: "+lives, canvas.width-65, 20);
+}
+
+collision = () => {
+    checkBallCollision();
+    brickCollision();
+}
+
+
 draw = () => {
     ctx.clearRect(0,0,canvas.width,canvas.height)
     drawBall();
     drawPaddle();
     drawBricks();
+    drawScore();
+    drawLives();
     moveBall();
     movePaddle();
+    requestAnimationFrame(draw);
 }
 
-setInterval(draw, 10);
-
+draw();
 
 
